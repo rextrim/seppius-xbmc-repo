2e68ef8d8e001e589f844d39abd46c6f774274d5
seppius2 <seppius2@6a205204-3e34-5d9b-f158-6e7a6818afab>
2010-10-24 16:13:53 +0000

    Вск Окт 24 20:18:48 MSD 2010

--- trunk/addons/plugin.video.rusd.tv/default.py
+++ trunk/addons/plugin.video.rusd.tv/default.py
@@ -479,6 +479,7 @@ if run_once():
 
 	elif mode == 'openSettings':
 		__settings__.openSettings()
+		__settings__.setSetting('cookie', '')
 		xbmc.sleep(50)
 		xbmc.executebuiltin('Container.Refresh')
 
