7a16f568ad8362a144a6954a059f8ef54250d673
seppius2 <seppius2@6a205204-3e34-5d9b-f158-6e7a6818afab>
2011-01-03 09:40:35 +0000

    Пнд Янв  3 12:41:07 MSK 2011

--- addons/plugin.video.guzei.tv/default.py
+++ addons/plugin.video.guzei.tv/default.py
@@ -21,12 +21,11 @@
 import xbmc, xbmcgui, xbmcaddon, xbmcplugin, urllib2, urllib, re, string, sys, os, traceback
 from urllib import urlretrieve, urlcleanup
 
-HEADER      = 'Opera/10.60 (X11; openSUSE 11.3/Linux i686; U; ru) Presto/2.6.30 Version/10.60'
-GUZEI_TV = 'GUZEI.TV'
+h = int(sys.argv[1])
+icon   = os.path.join(os.getcwd(), "play.png")
 
-
-handle = int(sys.argv[1])
-play_thumb   = os.path.join(os.getcwd(), "play.png")
+def showMessage(heading, message, times = 3000):
+    xbmc.executebuiltin('XBMC.Notification("%s", "%s", %s, "%s")'%(heading, message, times, icon))
 
 def get_params():
 	param=[]
@@ -60,124 +59,56 @@ def clean2(name):
 def get_list():
 	try:
 		req = urllib2.Request('http://guzei.com/live/tv/')
-		req.add_header('User-Agent', HEADER)
 		f = urllib2.urlopen(req)
 		a = f.read()
 		f.close()
 	except:
-		return
-	stage1 = re.compile('<ol>(.+?)</ol>', re.DOTALL).findall(a)[0]
-	if len(stage1) == 0:
-		return
-	stage2 = re.compile('<li>(.+?)</li>').findall(stage1)
+		showMessage('HTTP ERROR','Не могу открыть http://guzei.com/live/tv/')
+		return False
+	try:
+		stage2 = re.compile('<li>(.+?)</li>').findall(re.compile('<ol>(.+?)</ol>', re.DOTALL).findall(a)[0])
+	except:
+		showMessage('ERROR','Сбой при выборке элементов <ol> или <li>')
+		return False
 	if len(stage2) == 0:
-		return
+		showMessage('ERROR','Элементы <li> не найдены')
+		return False
 	for stage2x in stage2:
-		(r_rates, r_id, r_name, r_speed, r_city, r_plot) = re.compile('<span class="r">(.+?)</span>.*<a class="name" href=".*id=([0-9]*)">(.+?)</a>.*:(.+?):(.+?):(.+)').findall(stage2x + ' ')[0]
-		speeds = re.compile('<a class="name" href="(.+?)">(.+?)</a>').findall(stage2x)
-		spd_cnt = len(speeds)
-		if spd_cnt == 0:
-			return
-		r_rates = clean2(r_rates)
-		#r_id    = clean2(r_id)
-		r_name  = clean2(r_name)
-		r_speed = clean2(r_speed)
-		r_city  = clean2(r_city)
-		r_plot  = clean2(r_plot)
-		url = sys.argv[0] + "?id=" + str(r_id) + '&rate=' + str(r_rates) + \
-			'&name='+urllib.quote_plus(r_name)+'&city='+urllib.quote_plus(r_city)+ \
-			'&plot='+urllib.quote_plus(r_plot)+'&speed='+str(r_speed)
-		r_name = r_rates +' "'+r_name+'" ['+r_speed+'kbps, '+r_city+']'
-		listitem=xbmcgui.ListItem(r_name, iconImage=play_thumb, thumbnailImage=play_thumb)
-		listitem.setInfo(type="Video", infoLabels = {
-			"Title":	r_name,
-			"Studio":	r_city,
-			"Plot":		r_plot,
-			"Genre":	'speed ' + r_speed } )
-		xbmcplugin.addDirectoryItem(handle, url, listitem, False)
+		(r_rates, r_name, r_city) = re.compile('(.[0-9]*)\s*:\s*<span class="r">(.*?)</span>\s*:\s*\((.*?)\)\s*:\s').findall(stage2x)[0]
+		r_urls = re.compile('<a class="name" href="(.*?)">(.*?)</a>').findall(stage2x)
+		GTitle = '%s : %s : (%s) : '%(r_rates, r_name, r_city)
+		for URL, SPEED in r_urls:
+			t = GTitle + SPEED
+			i = xbmcgui.ListItem(t, iconImage=icon, thumbnailImage=icon)
+			i.setProperty('IsPlayable', 'true')
+			u  = sys.argv[0] + '?mode=PLAY'
+			u += '&url=%s'%urllib.quote_plus(URL)
+			xbmcplugin.addDirectoryItem(h, u, i)
+	xbmcplugin.endOfDirectory(h)
 
-def get_play(url, rate, name, city, plot, speed):
-	dialog = xbmcgui.Dialog()
-	#ok = dialog.ok('Sorry', 'Embed data not found!')
-
+def get_play(url):
 	try:
 		req = urllib2.Request(url)
-		req.add_header('User-Agent', HEADER)
 		f = urllib2.urlopen(req)
 		a = f.read()
 		f.close()
 	except:
-		dialog.ok('ERROR', 'Cannot open URL=%s' % url)
-		return
-
-	#raw_url = re.compile('<embed type="application/x-shockwave-flash".*file=(.*?)&').findall(a)
-	raw_url = re.compile('<embed(.*?)embed>', re.DOTALL).findall(a)
-	if len(raw_url) == 0:
-		dialog = xbmcgui.Dialog()
-		ok = dialog.ok('Sorry', '<embed> data not found!')
-		return
-
-
-
-	raw1 = re.compile('src="(.*?)"').findall(raw_url[0])
-	if len(raw1) == 0:
-		raw1 = re.compile('file=(.*?)&').findall(raw_url[0])
-		if len(raw1) == 0:
-			dialog.ok('ERROR', 'Player data not found. Sorry.' % url)
-			return
-
-	play_url = raw1[0]
-
-	playList = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
-	playList.clear()
-	listitem=xbmcgui.ListItem(name,iconImage=play_thumb,thumbnailImage=play_thumb)
-	listitem.setInfo(type="Video", infoLabels = {
-		"Title": 	name,
-		#"Studio": 	caption + '(City: '+ city + ')',
-		#"Plot": 	caption,
-		#"Artist":	GUZEI_TV,
-		"Genre": 	'TV' } )
-	playList.add(play_url, listitem)
-	player = xbmc.Player(xbmc.PLAYER_CORE_AUTO)
-	player.play(playList)
+		showMessage('HTTP ERROR','Не могу открыть %s'%url)
+		return False
+	try:
+		pblock = re.compile('player\("(.*?)"').findall(a)
+	except:
+		showMessage('ERROR','Не могу найти player(...)')
+		return False
+	i = xbmcgui.ListItem(path = pblock[0])
+	xbmcplugin.setResolvedUrl(h, True, i)
 
 params = get_params()
-chid = None
-try:
-	chid = params["id"]
-except:
-	pass
-
-if len(params) == 0:
-	get_list()
-	xbmcplugin.setPluginCategory(handle, GUZEI_TV)
-	xbmcplugin.endOfDirectory(handle)
-
-elif chid != None:
-	rate = 0
-	name = 'No name of a channel'
-	city = 'city is not specified'
-	plot = 'No description'
-	speed = 0
-	try:
-		rate = int(params["rate"])
-	except:
-		pass
-	try:
-		name = urllib.unquote_plus(params["name"])
-	except:
-		pass
-	try:
-		city = urllib.unquote_plus(params["city"])
-	except:
-		pass
-	try:
-		plot = urllib.unquote_plus(params["plot"])
-	except:
-		pass
-	try:
-		speed = int(params["speed"])
-	except:
-		pass
-	work_url = 'http://guzei.com/online_tv/watch.php?online_tv_id='+str(chid)
-	get_play(work_url, rate, name, city, plot, speed)
+mode   = None
+url    = ''
+try: mode    = urllib.unquote_plus(params['mode'])
+except: pass
+try: url     = urllib.unquote_plus(params['url'])
+except: pass
+if   mode == None:   get_list()
+elif mode == 'PLAY': get_play(url)
