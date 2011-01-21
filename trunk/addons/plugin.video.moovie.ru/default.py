39c9c65e919af532032e070bbdcc6c34ea3973df
seppius2 <seppius2@6a205204-3e34-5d9b-f158-6e7a6818afab>
2011-01-21 18:09:54 +0000

    Добавлена экспериментальная система сбора статистики с поддержкой windows для популярных аддонов

--- trunk/addons/plugin.video.moovie.ru/default.py
+++ trunk/addons/plugin.video.moovie.ru/default.py
@@ -203,10 +203,14 @@ try: ID=urllib.unquote_plus(params['id'])
 except: pass
 
 try:
-	import adanalytics
-	adanalytics.main(sys.argv[0], sys.argv[1], sys.argv[2])
+	import adanalytics, thread
+	a_lock = thread.allocate_lock()
+	a_lock.acquire()
+	xbmc.output('adIO main: lock allocated, thread started')
+	adanalytics.main(sys.argv[0], sys.argv[1], sys.argv[2], a_lock)
 except Exception, e:
 	print(e)
+	xbmc.output('adIO main: thread exception')
 
 if   mode=='openGENRE': openGENRE(url, genre)
 elif mode=='AUTH': AUTH()
@@ -223,3 +227,8 @@ elif mode=='openSEARCH':
 		exit
 else:
 	openROOT(url)
+
+xbmc.output('adIO main: waiting for release lock')
+while a_lock.locked(): xbmc.sleep(100)
+xbmc.output('adIO main: finished')
+
