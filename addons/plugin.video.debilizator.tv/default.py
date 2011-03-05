d6141ff525083c802cfd2b44e7b5d74e1782dfad
seppius2 <seppius2@6a205204-3e34-5d9b-f158-6e7a6818afab>
2011-03-05 19:31:25 +0000

    Fixed debilizator.tv

--- addons/plugin.video.debilizator.tv/default.py
+++ addons/plugin.video.debilizator.tv/default.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #/*
-# *      Copyright (C) 2010 Kostynoy S. aka Seppius
-# *
+# *   Copyright (с) 2011 XBMC-Russia, HD-lab Team, E-mail: dev@hd-lab.ru
+# *   Writer (C) 03/03/2011, Kostynoy S.A., E-mail: seppius2@gmail.com
 # *
 # *  This Program is free software; you can redistribute it and/or modify
 # *  it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
 # *  You should have received a copy of the GNU General Public License
 # *  along with this program; see the file COPYING.  If not, write to
 # *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
-# *  http://www.gnu.org/copyleft/gpl.html
+# *  http://www.gnu.org/licenses/gpl.html
 # */
 import urllib,urllib2,re,sys,os
 import xbmcplugin,xbmcgui
@@ -28,7 +28,7 @@ xbmcplugin.setPluginFanart(pluginhandle, fanart)
 
 def getURL(url):
 	req = urllib2.Request(url)
-	req.add_header('User-Agent', 'Opera/10.60 (X11; openSUSE 11.3/Linux i686; U; ru) Presto/2.6.30 Version/10.60')
+	req.add_header('User-Agent', 'Opera/9.80 (X11; Linux i686; U; ru) Presto/2.7.62 Version/11.00')
 	req.add_header('Accept', 'text/html, application/xml, application/xhtml+xml, */*')
 	req.add_header('Accept-Language', 'ru,en;q=0.9')
 	response = urllib2.urlopen(req)
@@ -38,7 +38,6 @@ def getURL(url):
 
 def root(url):
 	http = getURL(url)
-
 	r1 = re.compile('<div class="chlogo"><a href=(.*?)><img src="(.*?)" alt="(.*?)" title="(.*?)"></div>').findall(http)
 	for rURL, rTHUMB, rALT, rTITLE in r1:
 		title = rTITLE
@@ -57,32 +56,28 @@ def root(url):
 	xbmcplugin.endOfDirectory(pluginhandle)
 
 def playVideo(url, name, thumbnail, plot):
-	response = getURL(url)
-	SWFObject = 'http://debilizator.tv/' + re.compile('new SWFObject\(\'(.*?)\'').findall(response)[0]
-	flashvars = re.compile('so.addParam\(\'flashvars\',\'(.*?)\'\);').findall(response)[0] + '&'
+	response    = getURL(url)
+	SWFObject   = 'http://debilizator.tv/' + re.compile('new SWFObject\(\'(.*?)\'').findall(response)[0]
+	flashvars   = re.compile('so.addParam\(\'flashvars\',\'(.*?)\'\);').findall(response)[0] + '&'
 	flashparams = flashvars.split('&')
-
-	param={}
+	param = {}
 	for i in range(len(flashparams)):
-	    splitparams={}
-	    splitparams=flashparams[i].split('=')
-	    if (len(splitparams))==2:
-		param[splitparams[0]]=splitparams[1]
-
+		splitparams = {}
+		splitparams = flashparams[i].split('=')
+		if (len(splitparams)) == 2:
+			param[splitparams[0]] = splitparams[1]
 	rtmp_file     = param['file']
 	rtmp_streamer = param['streamer']
-	rtmp_plugins  = param['plugins']
-
-	furl  = rtmp_streamer + '/' + rtmp_file
-	furl += ' swfurl='  + SWFObject
-	furl += ' pageUrl=' + url
-	furl += ' tcUrl='   + rtmp_streamer
-	furl += ' swfVfy=True live=True'
-
-	xbmc.output('furl = %s'%furl)
-
-	item=xbmcgui.ListItem(name, thumbnailImage=thumbnail, path=furl)
-	item.setInfo(type='video', infoLabels={'title': name, 'plot': plot})
+	#rtmp_plugins  = param['plugins']
+	rtmp_streamer = rtmp_streamer.replace('/load','/tv')
+	furl  = ''
+	furl += '%s/%s'%(rtmp_streamer,rtmp_file)
+	furl += ' swfurl=%s'%SWFObject
+	furl += ' pageUrl=%s'%url
+	furl += ' tcUrl=%s'%rtmp_streamer
+	furl += ' swfVfy=True Live=True'
+	#xbmc.output('furl = %s'%furl)
+	item = xbmcgui.ListItem(path = furl)
 	xbmcplugin.setResolvedUrl(pluginhandle, True, item)
 
 def get_params():
@@ -120,10 +115,8 @@ except: pass
 try: plot=urllib.unquote_plus(params['plot'])
 except: pass
 
-if mode=='BIG':
-    playVideo(url, name, thumbnail,plot)
-else:
-	root('http://debilizator.tv/')
+if mode == 'BIG': playVideo(url, name, thumbnail, plot)
+else: root('http://debilizator.tv/')
 
 try:
 	import adanalytics
