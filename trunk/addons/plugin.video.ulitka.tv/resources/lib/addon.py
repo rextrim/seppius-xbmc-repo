f4507417b2e37d0b773e0149fa888a1fc4855178
seppius2 <seppius2@6a205204-3e34-5d9b-f158-6e7a6818afab>
2011-07-02 15:13:59 +0000

    Капитальный ремонт кода Ulitka.TV. Добавлена авторизация.

--- trunk/addons/plugin.video.ulitka.tv/resources/lib/addon.py
+++ trunk/addons/plugin.video.ulitka.tv/resources/lib/addon.py
@@ -19,7 +19,6 @@
 #   the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 #   http://www.gnu.org/licenses/gpl.html
 
-
 import sys, xbmc, xbmcgui, xbmcplugin, xbmcaddon, re
 import os, urllib, urllib2, httplib, xml.dom.minidom, cookielib
 
@@ -28,60 +27,100 @@ from html5lib import treebuilders
 
 from jsdecoder import unpack
 
-icon   = xbmc.translatePath(os.path.join(os.getcwd().replace(';', ''), 'icon.png'))
 h = int(sys.argv[1])
 
+__addon__ = xbmcaddon.Addon(id = 'plugin.video.ulitka.tv')
 
-addon_id       = 'unknown addon id'
-addon_name     = 'unknown addon'
-addon_version  = '0.0.0'
-addon_provider = 'unknown'
+addon_icon    = __addon__.getAddonInfo('icon')
+addon_fanart  = __addon__.getAddonInfo('fanart')
+addon_path    = __addon__.getAddonInfo('path')
+addon_type    = __addon__.getAddonInfo('type')
+addon_id      = __addon__.getAddonInfo('id')
+addon_author  = __addon__.getAddonInfo('author')
+addon_name    = __addon__.getAddonInfo('name')
+addon_version = __addon__.getAddonInfo('version')
+addon_profile = __addon__.getAddonInfo('profile')
 
-addon_xml = xbmc.translatePath(os.path.join(os.getcwd().replace(';', ''),'addon.xml'))
-if os.path.isfile(addon_xml):
-	af = open(addon_xml, 'r')
-	adom = xml.dom.minidom.parseString(af.read())
-	af.close()
-	areg = adom.getElementsByTagName('addon')
-	addon_id       = areg[0].getAttribute('id')
-	addon_name     = areg[0].getAttribute('name')
-	addon_version  = areg[0].getAttribute('version')
-	addon_provider = areg[0].getAttribute('provider-name')
+icon   = xbmc.translatePath(addon_icon)
+fanart = xbmc.translatePath(addon_fanart)
+profile = xbmc.translatePath(addon_profile)
+
 
 def showMessage(heading, message, times = 3000):
 	xbmc.executebuiltin('XBMC.Notification("%s", "%s", %s, "%s")'%(heading, message, times, icon))
 
-__settings__ = xbmcaddon.Addon(id=addon_id)
-__language__ = __settings__.getLocalizedString
 
+def GET(tu, post=None):
+	#print '[%s]: TARGET TU=[%s]' % (addon_id, tu)
+	#print '[%s]: POST DATA=[%s]' % (addon_id, post)
 
-def GET(tu):
-	#print 'GET [%s]' % tu
 	try:
+		CJ = cookielib.CookieJar()
+		urllib2.install_opener(urllib2.build_opener(urllib2.HTTPCookieProcessor(CJ)))
 		req = urllib2.Request(tu)
-		req.add_header(     'User-Agent','Opera/9.80 (X11; Linux i686; U; ru) Presto/2.8.131 Version/11.10')
-		req.add_header(         'Accept','text/html, application/xml, application/xhtml+xml, image/png, image/jpeg, image/gif, image/x-xbitmap, */*')
-		req.add_header('Accept-Language','ru-RU,ru;q=0.9,en;q=0.8')
-		req.add_header( 'Accept-Charset','utf-8, utf-16, *;q=0.1')
-		req.add_header('Accept-Encoding','identity, *;q=0')
-		req.add_header(     'Connection','Keep-Alive')
-		f = urllib2.urlopen(req)
+		req.add_header('User-Agent', '%s/%s %s/%s/%s' % (addon_type, addon_id, addon_author, addon_version, urllib.quote_plus(addon_name)))
+
+		if post: req.add_header('Content-Type', 'application/x-www-form-urlencoded')
+
+		cookie_path = os.path.join(profile, 'cookie')
+		if not os.path.exists(cookie_path):
+			os.makedirs(cookie_path)
+			print '[%s]: os.makedirs(cookie_path=%s)' % (addon_id, cookie_path)
+
+		cookie_send = {}
+		for cookie_fname in os.listdir(cookie_path):
+			cookie_file = os.path.join(cookie_path, cookie_fname)
+			if os.path.isfile(cookie_file):
+				cf = open(cookie_file, 'r')
+				cookie_send[os.path.basename(cookie_file)] = cf.read()
+				cf.close()
+			else: print '[%s]: NOT os.path.isfile(cookie_file=%s)' % (addon_id, cookie_file)
+
+		cookie_string = urllib.urlencode(cookie_send).replace('&','; ')
+		req.add_header('Cookie', cookie_string)
+		f = urllib2.urlopen(req, post)
+
+		for Cook in CJ:
+			cookie_file = os.path.join(cookie_path, Cook.name)
+			cf = open(cookie_file, 'w')
+			cf.write(Cook.value)
+			cf.close()
+
 		a = f.read()
 		f.close()
-		#print a
 		return a
 	except Exception, e:
+		print '[%s]: GET EXCEPTION: %s' % (addon_id, e)
 		showMessage(tu, e, 5000)
 		return None
+
+def auth(params):
+	username = __addon__.getSetting('username')
+	password = __addon__.getSetting('password')
+
+	if len(username) and len(password):
+		http = GET('http://www.ulitka.tv/')
+		if http != None:
+			post_data = {'username':username, 'passwd':password}
+			DT = html5lib.HTMLParser(tree=treebuilders.getTreeBuilder('dom')).parse(http)
+			for div0 in DT.getElementsByTagName('span'):
+				if div0.getAttribute('class') == 'login':
+					for inputs in div0.getElementsByTagName('input'):
+						if inputs.getAttribute('type') == 'hidden':
+							post_data[inputs.getAttribute('name')] = inputs.getAttribute('value')
+			GET('http://www.ulitka.tv/index.php', urllib.urlencode(post_data))
+			showMessage('Логин и пароль отправлен', 'Нет гарантии что она произошла', 5000)
+	else:
+		__addon__.openSettings()
 
 
 
 def showroot():
 	http = GET('http://www.ulitka.tv/')
 	if http != None:
-		li = xbmcgui.ListItem("[Последние]")
+		xbmcplugin.addDirectoryItem(h, '%s?%s' % (sys.argv[0], urllib.urlencode({'func':'auth'})), xbmcgui.ListItem('[ Авторизация ]'), False)
+		li = xbmcgui.ListItem('[ Последние ]')
 		xbmcplugin.addDirectoryItem(h, '%s?%s' % (sys.argv[0], urllib.urlencode({'func':'showlatest'})), li, True)
-
 		DT = html5lib.HTMLParser(tree=treebuilders.getTreeBuilder('dom')).parse(http)
 		for div0 in DT.getElementsByTagName('div'):
 			if div0.getAttribute('id') == 'menu':
@@ -98,6 +137,7 @@ def showroot():
 def showlatest(params):
 	http = GET('http://www.ulitka.tv/')
 	if http != None:
+#		xbmcplugin.addDirectoryItem(h, '%s?%s' % (sys.argv[0], urllib.urlencode({'func':'auth'})), xbmcgui.ListItem('[ Авторизация ]'), False)
 		DT = html5lib.HTMLParser(tree=treebuilders.getTreeBuilder('dom')).parse(http)
 		for div0 in DT.getElementsByTagName('div'):
 			if div0.getAttribute('class') == 'aidanews':
@@ -105,30 +145,24 @@ def showlatest(params):
 					img = td.getElementsByTagName('img')[0]
 					episode = img.getAttribute('alt').encode('utf8', 'ignore')
 					imgSrc = img.getAttribute('src').split('/')
-
 					icon = 'http://www.ulitka.tv/images/posters/' + imgSrc[-1]
 					show = str(imgSrc[-1]).replace('.jpg', '').replace('-', ' ')
 					href = None
-
-					#for span in td.getElementsByTagName('span'):
-					#	if show == 'Unknown' and span.getAttribute('style') == ' font-size: 115%;':
-					#		f.write(str(span.firstChild.data.encode('utf8')))
-
 					for a in td.getElementsByTagName('a'):
 						if a.getAttribute('title'):
 							href = a.getAttribute('href')
 							show = str(href.split('/')[-2]).replace('-', ' ')
-
 					li = xbmcgui.ListItem("%season %s" % (show, episode), iconImage = icon, thumbnailImage = icon)
 					li.setInfo(type = 'video', infoLabels = {'tvshowtitle':show})
 					li.setProperty('IsPlayable', 'true')
 					xbmcplugin.addDirectoryItem(h, '%s?%s' % (sys.argv[0], urllib.urlencode({'func':'watch', 'href': href})), li, False)
-
 		xbmcplugin.endOfDirectory(h)
+
 
 def showserials(params):
 	http = GET('http://www.ulitka.tv' + params['href'])
 	if http != None:
+#		xbmcplugin.addDirectoryItem(h, '%s?%s' % (sys.argv[0], urllib.urlencode({'func':'auth'})), xbmcgui.ListItem('[ Авторизация ]'), False)
 		DT = html5lib.HTMLParser(tree=treebuilders.getTreeBuilder('dom')).parse(http)
 		for div in DT.getElementsByTagName('td'):
 			iName = ''
@@ -147,10 +181,10 @@ def showserials(params):
 		xbmcplugin.endOfDirectory(h)
 
 
-
 def showserial(params):
 	http = GET('http://www.ulitka.tv' + params['href'])
 	if http != None:
+		#xbmcplugin.addDirectoryItem(h, '%s?%s' % (sys.argv[0], urllib.urlencode({'func':'auth'})), xbmcgui.ListItem('[ Авторизация ]'), False)
 		DT = html5lib.HTMLParser(tree=treebuilders.getTreeBuilder('dom')).parse(http)
 		for div in DT.getElementsByTagName('div'):
 			if div.getAttribute('class') == 'description':
@@ -172,10 +206,10 @@ def showserial(params):
 		xbmcplugin.endOfDirectory(h)
 
 
-
 def showepisodes(params):
 	http = GET('http://www.ulitka.tv' + params['href'])
 	if http != None:
+		#xbmcplugin.addDirectoryItem(h, '%s?%s' % (sys.argv[0], urllib.urlencode({'func':'auth'})), xbmcgui.ListItem('[ Авторизация ]'), False)
 		DT = html5lib.HTMLParser(tree=treebuilders.getTreeBuilder('dom')).parse(http)
 		for div in DT.getElementsByTagName('div'):
 			if div.getAttribute('class') == 'categorylist':
@@ -192,6 +226,7 @@ def showepisodes(params):
 								xbmcplugin.addDirectoryItem(h, '%s?%s' % (sys.argv[0], urllib.urlencode({'func':'watch', 'href':div3.getAttribute('href')})), li, False)
 		xbmcplugin.endOfDirectory(h)
 
+
 def watch(params):
 	http = GET('http://www.ulitka.tv' + params['href'])
 	if http != None:
@@ -200,14 +235,12 @@ def watch(params):
 		s = jsData[0]
 		s = s[s.find('}(')+1:-1]
 		initJs = eval('unpack' + s)
-
-		url = 'http://ww.ulitka.tv:8080'
-
-		fileRe = re.compile('so\.addVariable\("file",style\+"([^"]+)')
-		url = url + fileRe.findall(initJs)[0]
-
-		url = url + '?start=0&id=1&client=FLASH%20LNX%2010,2,159,1&version=4.3.132&width=640'
+		fileRe = re.compile('so\.addVariable\("file",style\+"([^"]+)').findall(initJs)[0]
+		swfRe_swf, swfRe_id = re.compile('SWFObject\("(.+?)","(.+?)".*?\);').findall(initJs)[0]
+		url = 'http://ww.ulitka.tv:8080'+fileRe+'?start=0&id='+swfRe_id+'&client=FLASH%20LNX%2010,3,181,26&version=4.3.132&width=640' # % (, )  # &id=0
+		#url += '|%s' % urllib.urlencode({'Referer':'http://www.ulitka.tv%s' % swfRe_swf, 'User-Agent':'Opera/9.80 (X11; Linux i686; U; ru) Presto/2.9.168 Version/11.50'})
 		i = xbmcgui.ListItem(path = url)
+		i.setProperty('mimetype', 'video/mp4')
 		xbmcplugin.setResolvedUrl(h, True, i)
 
 
