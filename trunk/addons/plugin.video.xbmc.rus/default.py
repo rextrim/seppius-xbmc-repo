b6c3f6aecd25ced0b43232f32129c4071ea75fce
seppius2 <seppius2@6a205204-3e34-5d9b-f158-6e7a6818afab>
2011-08-19 16:26:57 +0000

    XBMC Russia: Улучшена совместимость с XBMC 11

--- trunk/addons/plugin.video.xbmc.rus/default.py
+++ trunk/addons/plugin.video.xbmc.rus/default.py
@@ -2,10 +2,11 @@
 # Copyright (c) 2011 XBMC-Russia, HD-lab Team, E-mail: dev@hd-lab.ru
 # Writer (c) 2011, Kostynoy S.A., E-mail: seppius2@gmail.com
 
-import sys, os
+import sys, os, xbmcaddon
 
-sys.path.append(os.path.join(os.getcwd().replace(';', ''), 'resources', 'lib'))
+__addon__ = xbmcaddon.Addon( id = 'plugin.video.xbmc.rus' )
+sys.path.append( os.path.join( __addon__.getAddonInfo( 'path' ), 'resources', 'lib') )
 
-if (__name__ == "__main__" ):
+if (__name__ == '__main__' ):
 	import xbmcrussia
 	xbmcrussia.addon_main()
