c2c383231658d56d63d3127ab375a1c7d15fe8dd
seppius2 <seppius2@6a205204-3e34-5d9b-f158-6e7a6818afab>
2012-01-10 05:50:47 +0000

    ca.tanas.ruremtv: first added

---
+++ addons/ca.tanas.ruremtv/config.py
@@ -0,0 +1,11 @@
+import sys
+server          = 'iptv-distribution.net'
+epgService      = '/ds/epg/service.asmx'
+vodService      = '/ucas/service.asmx'
+vod2Service     = '/ds/vod/generic/service.asmx'
+radioService    = '/ucas/service.asmx'
+streamService   = '/ds/cas/streams/generic/stream.asmx'
+protocol        = 'mms'
+siteId          = '5'
+appName         = 'XBMC Plugin (' + sys.platform + ')'
+
