0cc38018c2cba1d1e1d22477af46ad3615dfeaee
nuismons@gmail.com <nuismons@gmail.com@6a205204-3e34-5d9b-f158-6e7a6818afab>
2012-08-17 14:45:37 +0000

    torrent stream module

---
+++ trunk/addons/plugin.video.torrent.gnu/resources/lib/torr2xbmc.py
@@ -0,0 +1,206 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import httplib
+import urllib
+import urllib2
+import re
+import sys
+import os
+import socket
+
+import xbmcplugin
+import xbmcgui
+import xbmcaddon
+import xbmc
+import xbmcaddon
+
+from TSCore import TSengine as tsengine
+import base64
+
+hos = int(sys.argv[1])
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+__addon__ = xbmcaddon.Addon( id = 'plugin.video.torrent.gnu' )
+__language__ = __addon__.getLocalizedString
+
+addon_icon    = __addon__.getAddonInfo('icon')
+addon_fanart  = __addon__.getAddonInfo('fanart')
+addon_path    = __addon__.getAddonInfo('path')
+addon_type    = __addon__.getAddonInfo('type')
+addon_id      = __addon__.getAddonInfo('id')
+addon_author  = __addon__.getAddonInfo('author')
+addon_name    = __addon__.getAddonInfo('name')
+addon_version = __addon__.getAddonInfo('version')
+
+ktv_folder=__addon__.getSetting('download_path')
+while not __addon__.getSetting('download_path'): __addon__.openSettings()
+ktv_folder=__addon__.getSetting('download_path')
+
+# JSON понадобится, когда будет несколько файлов в торренте
+try:
+	import json
+except ImportError:
+	try:
+		import simplejson as json
+		xbmc.log( '[%s]: Error import json. Uses module simplejson' % addon_id, 2 )
+	except ImportError:
+		try:
+			import demjson3 as json
+			xbmc.log( '[%s]: Error import simplejson. Uses module demjson3' % addon_id, 3 )
+		except ImportError:
+			xbmc.log( '[%s]: Error import demjson3. Sorry.' % addon_id, 4 )
+
+def construct_request(params):
+	return '%s?%s' % (sys.argv[0], urllib.urlencode(params))
+	
+def GET(target, post=None):
+	#print target
+	try:
+		req = urllib2.Request(url = target, data = post)
+		req.add_header('User-Agent', 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1) ; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET4.0C)')
+		resp = urllib2.urlopen(req)
+		http = resp.read()
+		resp.close()
+		return http
+	except Exception, e:
+		xbmc.log( '[%s]: GET EXCEPT [%s]' % (addon_id, e), 4 )
+		showMessage('HTTP ERROR', e, 5000)
+		
+def showMessage(heading, message, times = 3000, pics = addon_icon):
+	try: xbmc.executebuiltin('XBMC.Notification("%s", "%s", %s, "%s")' % (heading.encode('utf-8'), message.encode('utf-8'), times, pics.encode('utf-8')))
+	except Exception, e:
+		xbmc.log( '[%s]: showMessage: Transcoding UTF-8 failed [%s]' % (addon_id, e), 2 )
+		try: xbmc.executebuiltin('XBMC.Notification("%s", "%s", %s, "%s")' % (heading, message, times, pics))
+		except Exception, e:
+			xbmc.log( '[%s]: showMessage: exec failed [%s]' % (addon_id, e), 3 )
+def rutor(params):
+	#print 'dodododo'
+	http = GET('http://www.rutor.org/browse/0/1/0/2')
+	#print http
+	out=re.findall('http://d.rutor.org/download/[0-9]+',http)
+	for filename in out:
+		#print filename
+		li = xbmcgui.ListItem(filename)
+		uri = construct_request({
+				'func': 'play_url',
+				'file':filename
+			})
+		xbmcplugin.addDirectoryItem(hos, uri, li, True)
+	xbmcplugin.endOfDirectory(hos)
+def mainScreen(params):
+	li = xbmcgui.ListItem('rutor.top')
+	uri = construct_request({
+		'func': 'rutor'
+	})
+	xbmcplugin.addDirectoryItem(hos, uri, li, True)
+	path=ktv_folder
+	dirList=os.listdir(path)
+	for fname in dirList:
+		if re.search('[^/]+.torrent', fname):
+			torrlink='a'
+			li = xbmcgui.ListItem(fname)
+			uri = construct_request({
+				'func': 'play_file',
+				'file':fname
+			})
+			xbmcplugin.addDirectoryItem(hos, uri, li, True)
+	xbmcplugin.endOfDirectory(hos)
+
+def play_file(params):
+	#получаем содержимое файла в base64
+	filename=xbmc.translatePath(ktv_folder + params['file'])
+	f = open(filename, 'rb')
+	buf=f.read()
+	f.close
+	torr_link=base64.b64encode(buf)
+	
+	TSplayer=tsengine()
+	out=TSplayer.load_torrent(torr_link,'RAW')
+	if out=='Ok':
+		for k,v in TSplayer.files.iteritems():
+			li = xbmcgui.ListItem(urllib.unquote(k))
+			uri = construct_request({
+				'torr_url': torr_link,
+				'title': k,
+				'ind':v,
+				'img':None,
+				'func': 'play_it'
+			})
+			xbmcplugin.addDirectoryItem(hos, uri, li, False)
+	xbmcplugin.endOfDirectory(hos)
+	TSplayer.end()
+	
+def play_it(params):
+	torr_link=params['torr_url']	
+	TSplayer=tsengine()
+	out=TSplayer.load_torrent(torr_link,'RAW')
+	if out=='Ok':
+		TSplayer.play_url_ind(int(params['ind']),params['title'],addon_icon,params['img'])
+	TSplayer.end()
+	showMessage('Торрент', 'Стоп', 2000)    
+
+def play_url(params):
+	torr_link=params['file']
+	
+	TSplayer=tsengine()
+	out=TSplayer.load_torrent(torr_link,'TORRENT')
+	if out=='Ok':
+		for k,v in TSplayer.files.iteritems():
+			li = xbmcgui.ListItem(urllib.unquote(k))
+			uri = construct_request({
+				'torr_url': torr_link,
+				'title': k,
+				'ind':v,
+				'img':None,
+				'func': 'play_url2'
+			})
+			xbmcplugin.addDirectoryItem(hos, uri, li, False)
+	xbmcplugin.endOfDirectory(hos)
+	TSplayer.end()
+	
+def play_url2(params):
+	torr_link=params['torr_url']	
+	TSplayer=tsengine()
+	out=TSplayer.load_torrent(torr_link,'TORRENT')
+	if out=='Ok':
+		TSplayer.play_url_ind(int(params['ind']),params['title'],addon_icon,params['img'])
+	TSplayer.end()
+	showMessage('Торрент', 'Стоп', 2000)    
+		
+def get_params(paramstring):
+	param=[]
+	if len(paramstring)>=2:
+		params=paramstring
+		cleanedparams=params.replace('?','')
+		if (params[len(params)-1]=='/'):
+			params=params[0:len(params)-2]
+		pairsofparams=cleanedparams.split('&')
+		param={}
+		for i in range(len(pairsofparams)):
+			splitparams={}
+			splitparams=pairsofparams[i].split('=')
+			if (len(splitparams))==2:
+				param[splitparams[0]]=splitparams[1]
+	if len(param) > 0:
+		for cur in param:
+			param[cur] = urllib.unquote_plus(param[cur])
+	return param
+
+def addon_main():
+	#xbmc.log( '[s]: play' , 2 )
+	params = get_params(sys.argv[2])
+	try:
+		func = params['func']
+		del params['func']
+	except:
+		func = None
+		xbmc.log( '[%s]: Primary input' % addon_id, 1 )
+		mainScreen(params)
+	if func != None:
+		try: pfunc = globals()[func]
+		except:
+			pfunc = None
+			xbmc.log( '[%s]: Function "%s" not found' % (addon_id, func), 4 )
+			showMessage('Internal addon error', 'Function "%s" not found' % func, 2000)
+		if pfunc: pfunc(params)
---
+++ trunk/addons/script.module.torrent.ts/lib/TSCore.py
@@ -0,0 +1,292 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import httplib
+import urllib
+import urllib2
+import re
+import sys
+import os
+import socket
+import threading
+import time
+import random
+import json
+
+
+import xbmcgui
+import xbmc
+
+_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+addon_icon=None
+
+import TSEProgress as progress
+
+#Надо объедениить с TSengine!!!!!!!!!
+class myPlayer(xbmc.Player):
+	def __init__( self, *args, **kwargs ):
+		self.active = True
+		self.playl=xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
+		self.playl.clear()
+		self.title='None'
+		self.paused=None
+		self.resume=None
+		self.duration=None
+	def playlist_add( self, url, it):
+		self.playl.add(url=url, listitem= it)
+		
+	def play_start(self, ind=0):
+		self.play(self.playl)
+		self.playselected(ind)
+	def onPlayBackStarted( self ):
+		self.duration= int(xbmc.Player().getTotalTime()*1000)
+	def onPlayBackResumed( self ):
+		self.paused = False
+	def onPlayBackEnded( self ):
+		self.active = False
+	def onPlayBackStopped( self ):
+		self.active = False
+	def onPlayBackPaused( self ):
+		self.paused=True
+	
+class TSengine(object):
+	
+	def _TSpush(self,command):
+		#print '>>'+command
+		_sock.send(command+'\r\n')
+	
+	def __init__(self):
+		self.addon_icon=None
+		self.ready = False
+		self.filelist = None
+		self.files = {}
+		self.file_count = None
+		self.url=None
+		self.player=None
+		self.playing=False
+		self.error_num=None
+		self.dialog = progress.DownloadProgress()
+		self.dialog.create('Инициализация TS Engine', "")
+		self.timeout=10
+		self.mode=''
+	def load_torrent(self, torrent, mode, host='127.0.0.1', port=62062 ):
+		self.dialog.update(0,'Соединяюсь с TS Engine')
+		try:
+			_sock.connect((host, port))
+		except:
+			self.dialog.close()
+			return False
+			exit
+		self.mode=mode	
+		self.r = _TSpull(1)
+		self.r.start()
+		comm="HELLOBG"
+		self._TSpush(comm)
+		self.dialog.update(0,'Жду ответа от TS Engine')
+		timeout=self.timeout
+		while not self.r.last_com:
+			timeout=timeout-1
+			if timeout==0: break
+			time.sleep(1)
+		if timeout==0: 
+			self.dialog.update(100,'Ошибка загрузки торрента')
+			xbmc.sleep(2000)
+			self.dialog.close()
+			return 'No responce'
+		comm='READY'
+		self._TSpush(comm)
+		self.ready=True
+		self.dialog.update(100,'Передаю торрент')
+		self.url=torrent
+		comm='LOADASYNC '+ str(random.randint(0, 0x7fffffff)) +' '+mode+' ' + torrent+ ' 0 0 0'
+		self._TSpush(comm)
+		timeout=self.timeout
+		while not self.r.files:
+			timeout=timeout-1
+			if timeout==0: break
+			time.sleep(1)
+		if timeout==0: 
+			self.dialog.update(100,'Ошибка загрузки торрента')
+			xbmc.sleep(2000)
+			self.dialog.close()
+			return 'Load Failed'	
+		self.filelist=self.r.files
+		self.file_count = self.r.count
+		self.files={}
+		if self.file_count>1:
+			flist=json.loads(self.filelist)
+			for list in files['files']:
+				self.files[list[0]]=list[1]
+		elif self.file_count==1:
+			flist=json.loads(self.filelist)
+			list=flist['files'][0]
+			self.files[list[0]]=list[1]
+		#print self.files
+		#self.dialog.close()
+		return 'Ok'
+		
+
+	def play_url_ind(self, index=0, title='', icon=None, thumb=None):
+		self.dialog.update(0,'Получаю ссылку')
+		comm='START '+self.mode+ ' ' + self.url + ' '+ str(index) +' 0 0 0'
+		self._TSpush(comm)
+		off_timer=180
+		while not self.r.got_url:
+			if self.r.last_com=='STATUS':
+				try:
+					if self.r.state: self.dialog.update(self.r.progress,self.r.state,self.r.label)
+					#!!!!!!!!!тут буду использовать _com_received вместо ^^^
+					
+				except: pass
+				#print r.last_received
+				xbmc.sleep(1000)
+				off_timer=off_timer-1
+				if off_timer<=0: 
+					
+					break
+		if self.r.got_url:
+			plr=myPlayer()
+			lit= xbmcgui.ListItem(title, iconImage = thumb, thumbnailImage =thumb)
+			self.dialog.update(100,'Начинаю воспроизведение')
+			plr.play(self.r.got_url, lit)
+			while not plr.duration:
+				self.dialog.update(self.r.progress,self.r.state,self.r.label)
+				xbmc.sleep(1000)
+			self.dialog.close()
+			visible=False
+
+			
+			while plr.active:
+				#!!!!!!!!!1тут буду использовать _com_received
+				if plr.duration: 
+					comm='DUR '+self.r.got_url.replace('\r','').replace('\n','')+' '+str(plr.duration)
+					self._TSpush(comm)
+					plr.duration=None
+				
+				if plr.paused: 
+					print 'paused'
+					if not visible: 
+						print 'make window'
+						self.dialog = progress.DownloadProgress()
+						self.dialog.create('Инициализация TS Engine', "")
+						self.dialog.update(self.r.progress,self.r.state,self.r.label)
+						visible=True
+					if visible: self.dialog.update(self.r.progress,self.r.state,self.r.label)
+				elif visible:
+					print 'delete window'
+					self.dialog.close()
+					visible=None
+				#print self.r.state
+				xbmc.sleep(1000)
+			#self.end()
+		else: 
+			self.dialog.update(0,'Не дождался ссылки')
+			time.sleep(3)
+		try: self.dialog.close()
+		except: pass
+	def end(self):
+		#print self.r.received
+		
+		comm="SHUTDOWN"
+		self._TSpush(comm)
+		self.r.active = False
+		#self.r.end()
+		_sock.close()
+		#print 'Done'
+		try: self.dialog.close()
+		except: pass
+class _TSpull(threading.Thread):
+
+	def _com_received(self,text):
+		
+		#прогресс (буфферизация, скачано)
+		#сиды,пиры и скорости, если будут
+		#будет self.state=[progress,state,'seeds/peers']
+		try:
+			#self.state=None
+			#self.progress=0
+			#self.label=None
+			#print text
+			comm=text.split(' ')[1]
+			try:
+				ss=re.compile('main:[a-z]+',re.S)
+				s1=re.findall(ss, text)[0]
+				st=s1.split(':')[1]
+				#print st
+				if st=='prebuf': 
+					self.state='Буфферизация'
+					self.progress=int(text.split(';')[1])+0.1
+					self.label='Пиры:%s Скорость:%s'%(text.split(';')[8],text.split(';')[5])
+				if st=='dl': 
+					self.state='Скачивание'
+					self.progress=int(text.split(';')[1])+0.1
+					self.label='Пиры:%s Скорость:%s'%(text.split(';')[6],text.split(';')[3])
+				if st=='check': 
+					self.state='Проверка'
+					self.progress=int(text.split(';')[1])
+					self.label=None
+				if st=='idle': 
+					self.state='TS Engine бездействует'
+					self.progress=0
+				if st=='wait': 
+					self.state='TS Engine ожидает'
+					self.label='Секунд %ы'%(text.split(';')[1])
+					self.progress=0
+				#print self.state
+				#print self.label
+			except: pass
+			#print re.search('main:prebuf;',comm)
+			#print re.search('main:dl;',comm)
+			return text.split(' ')[0]
+		except: return text
+
+	def __init__(self,interval):
+		threading.Thread.__init__(self)
+		self.daemon = True
+		self.interval = interval	#Я не пользуюсь, возможно пригодится, если будет тормозить
+		self.active = True			#Если пошлем False - поток остановится и перестанет принимать данные
+		self.lastresolt=None		
+		self.received = []			#Тут хранится все, что пришло от сервера ТС (пригодится, я думаю)
+		self.last_received=None		#Последний ответ от ТССервера
+		self.last_com=None			#Последняя команда от ТССервера
+		self.got_url=None			#Будет ссылка на файл после буфферизации
+		self.files=None				#Список файлов в json
+		self.buffer=50000000			#размер буффера для приема нужен большой, если файлов много
+		self.count=None
+		self.state=''
+		self.label=''
+		self.progress=0
+	def run(self):
+		while self.active:
+			try:
+				self.last_received=_sock.recv(self.buffer)
+				#print self.last_received
+				#self.received.append(self.last_received)
+				self.last_com = self._com_received(self.last_received)
+				
+				if self.last_com=='PLAY': self.got_url=self.last_received.split(' ')[1] # если пришло PLAY URL, то забираем себе ссылку
+				if self.last_com=='LOADRESP': 
+					fil = self.last_received
+					ll= fil[fil.find('{'):len(fil)]
+					self.files=ll
+					#!!!!!!!!запихать файлы в {file:ind}
+					#print self.files
+					try:
+						json_files=json.loads(ll)
+						if json_files['status']==2:
+							self.count=len(json_files['files'])
+						if json_files['status']==1:
+							self.count=1
+						if json_files['status']==0:
+							self.count=None
+					except:
+						self.count=None
+						
+			except:
+				pass
+				
+	def end(self):
+		self.daemon = False
+
+
+		
