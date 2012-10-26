4c2fdf2cb84678f04df9ea501078ec680929d641
silen.xbmc <silen.xbmc@gmail.com>
2012-10-26 23:13:15 +0000

    SEASONVAR.RU: ver 2.1.0

--- addons/plugin.video.seasonvar.ru/resources/lib/xppod.py
+++ addons/plugin.video.seasonvar.ru/resources/lib/xppod.py
@@ -1,31 +1,77 @@
-﻿import base64
+﻿import re, os, urllib, urllib2, sys, urlparse
+import HTMLParser
+hpar = HTMLParser.HTMLParser()
 
-def Correction(path):
-    fname = path+'/code.dat'
-    f = open(fname, 'r')
-    ret = base64.b64decode(f.read())
-    f.close()
-    return ret, '<string>', 'exec'
+#---------- get web page -------------------------------------------------------
+def get_HTML(url, post = None, ref = None):
+    request = urllib2.Request(url, post)
 
+    host = urlparse.urlsplit(url).hostname
+    if ref==None:
+        ref='http://'+host
+
+    request.add_header('User-Agent', 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1) ; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET4.0C)')
+    request.add_header('Host',   host)
+    request.add_header('Accept', '*/*')
+    request.add_header('Accept-Language', 'ru-RU')
+    request.add_header('Referer',             ref)
+
+    try:
+        f = urllib2.urlopen(request)
+    except IOError, e:
+        if hasattr(e, 'reason'):
+           xbmc.log('We failed to reach a server.')
+        elif hasattr(e, 'code'):
+           xbmc.log('The server couldn\'t fulfill the request.')
+
+    html = f.read()
+
+    return html
 
 #-------------------------------------------------------------------------------
 # Юppod decoder (for seasonvar.ru)
 #-------------------------------------------------------------------------------
-
 def Decode(param):
+    #-- get hash keys
+    f = open(os.path.join(os.getcwd(), r'resources', r'lib', r'hash.key'), 'r')
+    hash_key = f.read()
+    f.close()
+
+    rez = Decode_String(param, hash_key)
+    if not 'html:' in rez:
+        #---- get new hash keys
+        url = 'http://justpaste.it/1gj7'
+        html = get_HTML(url)
+
+        code = re.compile('<div id="articleContent">(.+?)<div class="noteFotter">', re.MULTILINE|re.DOTALL).findall(html)[0]
+
+        hash_list = []
+        for rec in re.compile('<p>(.+?)<\/p>', re.MULTILINE|re.DOTALL).findall(code):
+            hash_list.append(rec)
+
+        #---- save new hash keys
+        swf = open(os.path.join(os.getcwd(), r'resources', r'lib', r'hash.key'), 'w')
+        swf.write(hash_list[0]+'\n'+hash_list[1])
+        swf.close()
+
+        #---
+        hask_key = hash_list[0]+'\n'+hash_list[1]
+        rez = Decode_String(param, hash_key)
+
+    return rez
+
+def Decode_String(param, hash_key):
     #-- define variables
     loc_3 = [0,0,0,0]
     loc_4 = [0,0,0]
     loc_2 = ''
+
     #-- define hash parameters for decoding
     dec = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
-    #hash1 = ["J", "p", "v", "n", "s", "R", "0", "3", "T", "m", "w", "u", "9", "x", "g", "a", "G", "L", "U", "X", "z", "t", "b", "7", "H", "="]
-    #hash2 = ["f", "N", "W", "5", "e", "l", "V", "D", "y", "Z", "I", "i", "M", "o", "Q", "1", "B", "8", "2", "6", "c", "d", "4", "Y", "k", "C"]
+    hash1 = hash_key.split('\n')[0]
+    hash2 = hash_key.split('\n')[1]
 
-    hash1 = ["J","p","v","n","s","R","0","3","T","m","w","u","9","x","g","a","G","L","U","X","z","t","b","7","H","="]
-    hash2 = ["f","N","W","5","e","l","V","D","y","Z","I","i","M","o","Q","1","B","8","2","6","c","d","4","Y","k","C"]
     #-- decode
-
     for i in range(0, len(hash1)):
         re1 = hash1[i]
         re2 = hash2[i]
