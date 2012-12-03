1eb204aa7e35e32357bb5100932fd83731852487
silen.xbmc@gmail.com <silen.xbmc@gmail.com@6a205204-3e34-5d9b-f158-6e7a6818afab>
2012-12-03 00:49:19 +0000

    SEASONVAR.RU: ver. 2.1.2

--- trunk/addons/plugin.video.seasonvar.ru/resources/lib/xppod.py
+++ trunk/addons/plugin.video.seasonvar.ru/resources/lib/xppod.py
@@ -55,51 +55,57 @@ def Decode(param):
         swf.close()
 
         #---
-        hask_key = hash_list[0]+'\n'+hash_list[1]
+        hash_key= hash_list[0]+'\n'+hash_list[1]
         rez = Decode_String(param, hash_key)
 
     return rez
 
 def Decode_String(param, hash_key):
-    #-- define variables
-    loc_3 = [0,0,0,0]
-    loc_4 = [0,0,0]
-    loc_2 = ''
+    try:
+        #-- define variables
+        loc_3 = [0,0,0,0]
+        loc_4 = [0,0,0]
+        loc_2 = ''
 
-    #-- define hash parameters for decoding
-    dec = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
-    hash1 = hash_key.split('\n')[0]
-    hash2 = hash_key.split('\n')[1]
+        #-- define hash parameters for decoding
+        dec = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
+        hash1 = hash_key.split('\n')[0]
+        hash2 = hash_key.split('\n')[1]
 
-    #-- decode
-    for i in range(0, len(hash1)):
-        re1 = hash1[i]
-        re2 = hash2[i]
+        print hash1
+        print hash2
 
-        param = param.replace(re1, '___')
-        param = param.replace(re2, re1)
-        param = param.replace('___', re2)
+        #-- decode
+        for i in range(0, len(hash1)):
+            re1 = hash1[i]
+            re2 = hash2[i]
 
-    i = 0
-    while i < len(param):
-        j = 0
-        while j < 4 and i+j < len(param):
-            loc_3[j] = dec.find(param[i+j])
-            j = j + 1
+            param = param.replace(re1, '___')
+            param = param.replace(re2, re1)
+            param = param.replace('___', re2)
 
-        loc_4[0] = (loc_3[0] << 2) + ((loc_3[1] & 48) >> 4);
-        loc_4[1] = ((loc_3[1] & 15) << 4) + ((loc_3[2] & 60) >> 2);
-        loc_4[2] = ((loc_3[2] & 3) << 6) + loc_3[3];
+        i = 0
+        while i < len(param):
+            j = 0
+            while j < 4 and i+j < len(param):
+                loc_3[j] = dec.find(param[i+j])
+                j = j + 1
 
-        j = 0
-        while j < 3:
-            if loc_3[j + 1] == 64:
-                break
+            loc_4[0] = (loc_3[0] << 2) + ((loc_3[1] & 48) >> 4);
+            loc_4[1] = ((loc_3[1] & 15) << 4) + ((loc_3[2] & 60) >> 2);
+            loc_4[2] = ((loc_3[2] & 3) << 6) + loc_3[3];
 
-            loc_2 += unichr(loc_4[j])
+            j = 0
+            while j < 3:
+                if loc_3[j + 1] == 64:
+                    break
 
-            j = j + 1
+                loc_2 += unichr(loc_4[j])
 
-        i = i + 4;
+                j = j + 1
+
+            i = i + 4;
+    except:
+        pass
 
     return loc_2
