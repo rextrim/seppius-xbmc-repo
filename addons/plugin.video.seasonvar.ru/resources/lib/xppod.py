34e6406739000cbe6833e564063f3a4e7d73ed2a
silen.xbmc <silen.xbmc@gmail.com>
2012-12-06 15:31:32 +0000

    SEASONVAR.RU: ver. 2.1.5

--- addons/plugin.video.seasonvar.ru/resources/lib/xppod.py
+++ addons/plugin.video.seasonvar.ru/resources/lib/xppod.py
@@ -8,6 +8,7 @@ class XPpod():
     #----------------- init ----------------------------------------------------
     def __init__(self, Addon):
         self.Addon = Addon
+        self.url = []
 
     #---------- get web page -------------------------------------------------------
     def get_HTML(self, url, post = None, ref = None):
@@ -46,67 +47,80 @@ class XPpod():
 
         rez = self.Decode_String(param, hash_key)
         if not 'html:' in rez:
-            #---- get new hash keys
-            url = 'http://justpaste.it/1gj7'
+            #-- hash servers
+            url = 'http://justpaste.it/xbmc_list'
             html = self.get_HTML(url)
-
             code = re.compile('<div id="articleContent">(.+?)<div class="noteFotter">', re.MULTILINE|re.DOTALL).findall(html)[0]
 
-            hash_list = []
             for rec in re.compile('<p>(.+?)<\/p>', re.MULTILINE|re.DOTALL).findall(code):
-                hash_list.append(rec)
+                self.url.append(rec)
 
-            #---- save new hash keys
-            swf = open(xbmc.translatePath(os.path.join(self.Addon.getAddonInfo('path'), r'resources', r'lib', r'hash.key')), 'w')
-            swf.write(hash_list[0]+'\n'+hash_list[1])
-            swf.close()
+            #---- get new hash keys
+            for url in self.url:
 
-            #---
-            hask_key = hash_list[0]+'\n'+hash_list[1]
-            rez = self.Decode_String(param, hash_key)
+                html = self.get_HTML(url)
+                code = re.compile('<div id="articleContent">(.+?)<div class="noteFotter">', re.MULTILINE|re.DOTALL).findall(html)[0]
 
+                hash_list = []
+                for rec in re.compile('<p>(.+?)<\/p>', re.MULTILINE|re.DOTALL).findall(code):
+                    hash_list.append(rec)
+
+                #-- assemble hash key
+                hash_key = hash_list[0]+'\n'+hash_list[1]
+                rez = self.Decode_String(param, hash_key)
+
+                if 'html:' in rez:
+                    #-- save new hash keys
+                    swf = open(xbmc.translatePath(os.path.join(self.Addon.getAddonInfo('path'), r'resources', r'lib', r'hash.key')), 'w')
+                    swf.write(hash_list[0]+'\n'+hash_list[1])
+                    swf.close()
+                    #-- exit from search
+                    break
         return rez
 
     def Decode_String(self, param, hash_key):
-        #-- define variables
-        loc_3 = [0,0,0,0]
-        loc_4 = [0,0,0]
-        loc_2 = ''
+        try:
+            #-- define variables
+            loc_3 = [0,0,0,0]
+            loc_4 = [0,0,0]
+            loc_2 = ''
 
-        #-- define hash parameters for decoding
-        dec = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
-        hash1 = hash_key.split('\n')[0]
-        hash2 = hash_key.split('\n')[1]
+            #-- define hash parameters for decoding
+            dec = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
+            hash1 = hash_key.split('\n')[0]
+            hash2 = hash_key.split('\n')[1]
 
-        #-- decode
-        for i in range(0, len(hash1)):
-            re1 = hash1[i]
-            re2 = hash2[i]
+            #-- decode
+            for i in range(0, len(hash1)):
+                re1 = hash1[i]
+                re2 = hash2[i]
 
-            param = param.replace(re1, '___')
-            param = param.replace(re2, re1)
-            param = param.replace('___', re2)
+                param = param.replace(re1, '___')
+                param = param.replace(re2, re1)
+                param = param.replace('___', re2)
 
-        i = 0
-        while i < len(param):
-            j = 0
-            while j < 4 and i+j < len(param):
-                loc_3[j] = dec.find(param[i+j])
-                j = j + 1
+            i = 0
+            while i < len(param):
+                j = 0
+                while j < 4 and i+j < len(param):
+                    loc_3[j] = dec.find(param[i+j])
+                    j = j + 1
 
-            loc_4[0] = (loc_3[0] << 2) + ((loc_3[1] & 48) >> 4);
-            loc_4[1] = ((loc_3[1] & 15) << 4) + ((loc_3[2] & 60) >> 2);
-            loc_4[2] = ((loc_3[2] & 3) << 6) + loc_3[3];
+                loc_4[0] = (loc_3[0] << 2) + ((loc_3[1] & 48) >> 4);
+                loc_4[1] = ((loc_3[1] & 15) << 4) + ((loc_3[2] & 60) >> 2);
+                loc_4[2] = ((loc_3[2] & 3) << 6) + loc_3[3];
 
-            j = 0
-            while j < 3:
-                if loc_3[j + 1] == 64:
-                    break
+                j = 0
+                while j < 3:
+                    if loc_3[j + 1] == 64:
+                        break
 
-                loc_2 += unichr(loc_4[j])
+                    loc_2 += unichr(loc_4[j])
 
-                j = j + 1
+                    j = j + 1
 
-            i = i + 4;
+                i = i + 4;
+        except:
+            loc_2 = ''
 
         return loc_2
