b8143eb6e62b8c7e2d4112ba6006e738ca21f7a9
silen.xbmc@gmail.com <silen.xbmc@gmail.com@6a205204-3e34-5d9b-f158-6e7a6818afab>
2012-12-29 18:42:58 +0000

    FEPCOM.NET: ver 1.1.4

--- trunk/addons/plugin.video.igru.net.ua/resources/lib/xppod.py
+++ trunk/addons/plugin.video.igru.net.ua/resources/lib/xppod.py
@@ -1,5 +1,5 @@
 ﻿#-------------------------------------------------------------------------------
-# Юppod decoder (for igru.net.ua)
+# Юppod decoder (for serialu.net)
 #-------------------------------------------------------------------------------
 
 def Decode(param):
@@ -9,8 +9,8 @@ def Decode(param):
     loc_2 = ''
     #-- define hash parameters for decoding
     dec = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
-    hash1 = ["L", "y", "c", "X", "2", "M", "a", "l", "p", "5", "Q", "e", "R", "t", "Z", "Y", "9", "m", "d", "0", "s", "V", "b", "3", "7", "="]
-    hash2 = ["i", "B", "v", "U", "H", "4", "D", "n", "k", "8", "x", "T", "u", "G", "w", "f", "N", "J", "6", "W", "1", "g", "z", "o", "I", "r"]
+    hash1 = ["0", "5", "u", "w", "6", "n", "H", "o", "B", "p", "N", "M", "D", "R", "z", "G", "V", "e", "i", "3", "m", "W", "U", "7", "g", "="]
+    hash2 = ["c", "T", "I", "4", "Q", "Z", "v", "Y", "y", "X", "k", "b", "8", "a", "J", "d", "1", "x", "L", "t", "l", "2", "f", "s", "9", "h"]
 
     #-- decode
     for i in range(0, len(hash1)):
@@ -36,9 +36,10 @@ def Decode(param):
         while j < 3:
             if loc_3[j + 1] == 64:
                 break
-
-            loc_2 += unichr(loc_4[j])
-
+            try:
+                loc_2 += unichr(loc_4[j])
+            except:
+                pass
             j = j + 1
 
         i = i + 4;
