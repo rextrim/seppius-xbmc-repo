ff591a30c3e1a864af44dba4174fb3cf544dac1e
07pov23@gmail.com <07pov23@gmail.com@6a205204-3e34-5d9b-f158-6e7a6818afab>
2013-02-11 12:40:08 +0000

    02/11/2013 16:40:08

--- trunk/addons/addons_xml_generator.py
+++ trunk/addons/addons_xml_generator.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+﻿#!/usr/bin/python
 """ addons.xml generator """
 
 import os, md5
@@ -35,7 +35,7 @@ class Generator:
                     # skip encoding format line
                     if ( line.find( "<?xml" ) >= 0 ): continue
                     # add line
-                    addon_xml += unicode( line.rstrip() + "\n", "utf-8" )
+                    addon_xml += line.rstrip() + "\n"
                 # we succeeded so add to our final addons.xml text
                 addons_xml += addon_xml.rstrip() + "\n\n"
             except Exception, e:
