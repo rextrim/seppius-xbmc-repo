ed06d7aef217132e35e8b54ef253ed40e880a405
Silhouette2022 <Silhouette2022@6a205204-3e34-5d9b-f158-6e7a6818afab>
2013-02-15 22:13:18 +0000

    added authorization for inetcom.tv

--- trunk/addons/plugin.video.inetcom.tv/default.py
+++ trunk/addons/plugin.video.inetcom.tv/default.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
 # Writer (c) 2012, Silhouette, E-mail: 
-# Rev. 0.2.1
+# Rev. 0.2.3
 
 
 
@@ -14,12 +14,20 @@ __addon__       = xbmcaddon.Addon(id='plugin.video.inetcom.tv')
 #fanart    = xbmc.translatePath( __addon__.getAddonInfo('path') + 'fanart.jpg')
 #xbmcplugin.setPluginFanart(pluginhandle, fanart)
 
+__settings__ = xbmcaddon.Addon(id='plugin.video.inetcom.tv')
+usr_log = __settings__.getSetting('usr_log')
+usr_pwd = __settings__.getSetting('usr_pwd')
+
+
 INC_url = 'http://inetcom.tv'
+INC_url2 = 'http://inetcom.tv/'
+INC_urlw = 'http://www.inetcom.tv'
 INC_ch = '/channel/all'
 INC_pr = '/tvprogram/index'
 INC_cat = '/tvprogram/ajaxShowChannels/cat/'
 INC_date = '/date/'
 INC_ajch = '/tvprogram/ajaxShowChannel/ch/'
+INC_auth = '/auth/login'
 
 
 dbg = 0
@@ -27,6 +35,9 @@ def dbg_log(line):
   if dbg: xbmc.log(line)
 
 def get_url(url, data = None, cookie = None, save_cookie = False, referrer = None, accept = None):
+
+    dbg_log('-get_url')
+    dbg_log(url)
     req = urllib2.Request(url)
     req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 5.1; rv:16.0) Gecko/20100101 Firefox/16.0')
     if accept == None: req.add_header('Accept', 'text/html, application/xml, application/xhtml+xml, */*')
@@ -45,8 +56,13 @@ def get_url(url, data = None, cookie = None, save_cookie = False, referrer = Non
     link=response.read()
     if save_cookie:
         setcookie = response.info().get('Set-Cookie', None)
+        #print "Set-Cookie: %s" % repr(setcookie)
+        #print response.info()['set-cookie']
         if setcookie:
-            setcookie = re.search('([^=]+=[^=;]+)', setcookie).group(1)
+            try:
+              setcookie = response.info()['set-cookie']
+            except:
+              pass
             link = link + '<cookie>' + setcookie + '</cookie>'
     
     response.close()
@@ -57,28 +73,40 @@ def INC_start():
     
     dbg_log('-INC_start')
 
-    if 1:
-        name='Live TV'
-        item = xbmcgui.ListItem(name)
-        uri = sys.argv[0] + '?mode=live'
-        xbmcplugin.addDirectoryItem(pluginhandle, uri, item, True)
-    
-        name='Archives'
-        item = xbmcgui.ListItem(name)
-        uri = sys.argv[0] + '?mode=chls'
-        xbmcplugin.addDirectoryItem(pluginhandle, uri, item, True)  
-    
-        xbmcplugin.endOfDirectory(pluginhandle)  
-    else:
-        DTV_online(DTV_url, 'prls')
+    http = get_url(INC_urlw + '/index.php', save_cookie = True)
+    mycookie = re.search('<cookie>(.+?)</cookie>', http).group(1)
+    http = get_url(INC_urlw + INC_auth, 
+                  data = "LoginForm%5Busername%5D=" + urllib.quote_plus(usr_log) + "&LoginForm%5Bpassword%5D=" + urllib.quote_plus(usr_pwd),
+                  cookie = mycookie, save_cookie = True, referrer = INC_urlw + '/index.php', accept='json')
+    mycookie = re.search('<cookie>(.+?)</cookie>', http).group(1)
+                  
+                  
+    http = get_url(INC_urlw + '/index.php', cookie = mycookie)
+    #mycookie = re.search('<cookie>(.+?)</cookie>', http).group(1)                  
+
+    name='Live TV'
+    item = xbmcgui.ListItem(name)
+    uri = sys.argv[0] + '?mode=live' + '&cook=' + urllib.quote_plus(mycookie)
+    xbmcplugin.addDirectoryItem(pluginhandle, uri, item, True)
+
+    name='Archives'
+    item = xbmcgui.ListItem(name)
+    uri = sys.argv[0] + '?mode=chls' + '&cook=' + urllib.quote_plus(mycookie)
+    xbmcplugin.addDirectoryItem(pluginhandle, uri, item, True)  
+
+    xbmcplugin.endOfDirectory(pluginhandle)  
+
         
                 
-def INC_live(url):
+def INC_live(url, mycookie):
     dbg_log('INC_live')
     
-    http = get_url(url, save_cookie = True, referrer = INC_url)
-    mycookie = re.search('<cookie>(.+?)</cookie>', http).group(1)
-    dbg_log(mycookie)
+    http = get_url(url, cookie = mycookie, referrer = INC_urlw + '/index.php')
+
+#    guest = re.compile('var isGuest=(.+?);').findall(http)
+#    print '--GUEST--'
+#    print guest
+      
     oneline = re.sub('[\r\n]', ' ', http)
     pr_ls = re.compile('<tr > +?<td class="col01"><a href="(.+?)"><img src="(.+?)"  alt="(.+?)" /></a></td>').findall(oneline)
 
@@ -88,7 +116,7 @@ def INC_live(url):
             dbg_log(name)
             item = xbmcgui.ListItem(name, iconImage=INC_url + logo, thumbnailImage=INC_url + logo)
             uri = sys.argv[0] + '?mode=play'
-            uri += '&url='+urllib.quote_plus(INC_url + href) + '&cook=' + urllib.quote_plus(mycookie)
+            uri += '&url='+urllib.quote_plus(INC_urlw + href) + '&cook=' + urllib.quote_plus(mycookie)
             item.setInfo( type='video', infoLabels={'title': name, 'plot': descr})
             item.setProperty('IsPlayable', 'true')
             dbg_log(uri)
@@ -99,7 +127,7 @@ def INC_live(url):
 
 def INC_play(url, mycookie):
     dbg_log('-INC_play')
-    response = get_url(url, cookie = mycookie, referrer = INC_url + INC_ch)
+    response = get_url(url, cookie = mycookie, referrer = INC_urlw + INC_ch)
     lnks_ls = re.compile("lnks = \['(.+?)'\];").findall(response)
 
     if len(lnks_ls):
@@ -109,11 +137,11 @@ def INC_play(url, mycookie):
 
 
 
-def INC_chls():
+def INC_chls(mycookie):
     dbg_log('-INC_chls')
-    http = get_url(INC_url + INC_ch, save_cookie = True, referrer = INC_url)
-    mycookie = re.search('<cookie>(.+?)</cookie>', http).group(1)
-    dbg_log(mycookie)
+    http = get_url(INC_url + INC_ch, cookie = mycookie,  referrer = INC_url)
+#    mycookie = re.search('<cookie>(.+?)</cookie>', http).group(1)
+#    dbg_log(mycookie)
     oneline = re.sub('[\r\n]', ' ', http)
     pr_ls = re.compile('<tr > +?<td class="col01"><a href="(.+?)"><img src="(.+?)"  alt="(.+?)" /></a></td>').findall(oneline)
     print pr_ls
@@ -259,8 +287,8 @@ except: pass
 
 
 if mode == 'play': INC_play(url, cook)
-elif mode == 'live': INC_live(INC_url + INC_ch)
-elif mode == 'chls': INC_chls()
+elif mode == 'live': INC_live(INC_urlw + INC_ch, cook)
+elif mode == 'chls': INC_chls(cook)
 elif mode == 'dtls': INC_dtls(url, name, thumbnail, cook)
 elif mode == 'prls': INC_prls(url, id, name, pdate, thumbnail, cook)
 elif mode == 'arpl': INC_arpl(url, id, name, pdate, ptime)
