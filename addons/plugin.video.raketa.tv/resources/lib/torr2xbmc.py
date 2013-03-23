b1e858a27e7adecf1e091689f7578792b17a3377
07pov23 <07pov23@gmail.com>
2013-03-23 19:45:47 +0000

    1.0

--- addons/plugin.video.raketa.tv/resources/lib/torr2xbmc.py
+++ addons/plugin.video.raketa.tv/resources/lib/torr2xbmc.py
@@ -14,31 +14,31 @@ import xbmcaddon
 import xbmc
 import xbmcaddon
 import datetime
-from BeautifulSoup import BeautifulSoup, BeautifulStoneSoup
 from TSCore import TSengine as tsengine
 import base64
+
 hos = int(sys.argv[1])
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 __addon__ = xbmcaddon.Addon( id = 'plugin.video.raketa.tv' )
 __language__ = __addon__.getLocalizedString
-addon_icon    = __addon__.getAddonInfo('icon')
+addon_icon	 = __addon__.getAddonInfo('icon')
 addon_fanart  = __addon__.getAddonInfo('fanart')
-addon_path    = __addon__.getAddonInfo('path')
-addon_type    = __addon__.getAddonInfo('type')
-addon_id      = __addon__.getAddonInfo('id')
+addon_path	 = __addon__.getAddonInfo('path')
+addon_type	 = __addon__.getAddonInfo('type')
+addon_id		= __addon__.getAddonInfo('id')
 addon_author  = __addon__.getAddonInfo('author')
-addon_name    = __addon__.getAddonInfo('name')
+addon_name	 = __addon__.getAddonInfo('name')
 addon_version = __addon__.getAddonInfo('version')
-ktv_folder=unicode(__addon__.getSetting('download_path'),'utf-8')
 prt_file=__addon__.getSetting('port_path')
 aceport=62062
-cookie = ""
 PLUGIN_DATA_PATH = xbmc.translatePath( os.path.join( "special://profile/addon_data", 'plugin.video.raketa.tv') )
 if (sys.platform == 'win32') or (sys.platform == 'win64'):
 	PLUGIN_DATA_PATH = PLUGIN_DATA_PATH.decode('utf-8')
+
 xbmcplugin.setContent(int(sys.argv[1]), 'movies')
+
 try:
-	if prt_file: 
+	if prt_file:  
 		gf = open(prt_file, 'r')
 		aceport=int(gf.read())
 		gf.close()
@@ -54,23 +54,6 @@ if not prt_file:
 		print aceport
 	except: aceport=62062
 
-while not __addon__.getSetting('download_path'): __addon__.openSettings()
-ktv_folder=unicode(__addon__.getSetting('download_path'),'utf-8')
-
-# JSON понадобится, когда будет несколько файлов в торренте
-try:
-	import json
-except ImportError:
-	try:
-		import simplejson as json
-		xbmc.log( '[%s]: Error import json. Uses module simplejson' % addon_id, 2 )
-	except ImportError:
-		try:
-			import demjson3 as json
-			xbmc.log( '[%s]: Error import simplejson. Uses module demjson3' % addon_id, 3 )
-		except ImportError:
-			xbmc.log( '[%s]: Error import demjson3. Sorry.' % addon_id, 4 )
-
 def construct_request(params):
 	return '%s?%s' % (sys.argv[0], urllib.urlencode(params))
 	
@@ -79,7 +62,6 @@ def GET(target, post=None):
 		req = urllib2.Request(url = target, data = post)
 		req.add_header('User-Agent', 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1) ; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET4.0C)')
 		resp = urllib2.urlopen(req)
-
 		http = resp.read()
 		resp.close()
 		return http
@@ -87,7 +69,7 @@ def GET(target, post=None):
 		xbmc.log( '[%s]: GET EXCEPT [%s]' % (addon_id, e), 4 )
 		showMessage('HTTP ERROR', e, 5000)
 		
-def showMessage(heading, message, times = 3000, pics = addon_icon):
+def showMessage(heading='Raketa-TV', message = '', times = 3000, pics = addon_icon):
 	try: xbmc.executebuiltin('XBMC.Notification("%s", "%s", %s, "%s")' % (heading.encode('utf-8'), message.encode('utf-8'), times, pics.encode('utf-8')))
 	except Exception, e:
 		xbmc.log( '[%s]: showMessage: Transcoding UTF-8 failed [%s]' % (addon_id, e), 2 )
@@ -95,136 +77,180 @@ def showMessage(heading, message, times = 3000, pics = addon_icon):
 		except Exception, e:
 			xbmc.log( '[%s]: showMessage: exec failed [%s]' % (addon_id, e), 3 )
 
-def Log(str):
-	import datetime
-	output = open(PLUGIN_DATA_PATH + '/log.txt', 'a')
-	output.write('%s: %s\r\n' % (datetime.datetime.now(), str))
-	output.close()
-		
-def play_ch(params):
-	try:
-		m=params['file'].find('http:')
-		if m == 0:
-			TSplayer=tsengine()
-			out=TSplayer.load_torrent(params['file'],'TORRENT',port=aceport)
-			if out=='Ok':
-				TSplayer.play_url_ind(0,params['title'],addon_icon,params['img'])
-			TSplayer.end()
-			showMessage('Torrent', 'Stop', 2000)
-		else:
-			ID = params['file']
-			try:
-				TSplayer=tsengine()
-				out=TSplayer.load_torrent(ID,'PID',port=aceport)
-				if out=='Ok':
-					TSplayer.play_url_ind(0,params['title'],addon_icon,params['img'])
-				TSplayer.end()
-			except Exception, e:
-				showMessage('Torrent', e)
-			showMessage('Torrent', 'Stop', 2000)
-	except Exception, e:
-			showMessage('Raketa TV', e)
+db_name = os.path.join(PLUGIN_DATA_PATH, 'tvbase.db')
 
-def GetJsonChannels(params, new = False):
-	data = ''
-	try:
-		if not new:
-			if os.path.exists(PLUGIN_DATA_PATH + '/channels_list.json'):
-				output = open(PLUGIN_DATA_PATH + '/channels_list.json', 'r')
-				data = json.loads(output.read())
-				output.close()
-				return data
-		
-		output = open('%s/channels_list.json' % PLUGIN_DATA_PATH, 'w')
-		data = GET('http://raketa-tv.com/player/JSON/channels_list.json');
-		output.write(data)
-		output.close()
-		data = json.loads(data)
-		
-	except Exception, e:
-		showMessage('Error', e, 10000)
-	return data
+def GetScript(params):
+	import time
+	xbmc.executebuiltin( "ActivateWindow(%d)" % ( 10147 ) )
+	window = xbmcgui.Window( 10147 )
 
-def GetProgram(param):
-	if param['id'] == '':
-		return
-	curDay = datetime.date.today().strftime("%d");
-	if not os.path.exists(PLUGIN_DATA_PATH + '/%s_program_list.json' % curDay):
+	db = DataBase(db_name)
+	sch = None
+	if params.has_key('id'):
 		try:
-			import glob
-			filelist = glob.glob(PLUGIN_DATA_PATH + "/*_program_list.json")
-			for f in filelist:
-				os.remove(f)
-		except : pass
-		output = open(PLUGIN_DATA_PATH + '/%s_program_list.json' % curDay, 'w')
-		output.write(GET('http://raketa-tv.com/player/JSON/program_list.json'))
-		output.close()
-
-	try:
-		import time
-		xbmc.executebuiltin( "ActivateWindow(%d)" % ( 10147 ) ) 
-		window = xbmcgui.Window( 10147 )
-		input = open(PLUGIN_DATA_PATH + '/%s_program_list.json' % curDay, 'r')
-		data = input.read().encode('utf-8')
-		data = json.loads(data)
-		txtProgram = ''
-		program = filter(lambda x: x['channel_number'] == param['id'], data)[0]
-		for chanel in program['program']:
-			startTime = time.localtime(float(chanel['ut_start']))
-			endTime = time.localtime(float(chanel['ut_stop']))
-			curTime = time.localtime()
-			if (endTime > curTime):
-				txtProgram = txtProgram + '%.2d:%.2d - %.2d:%.2d: %s\n' % (startTime.tm_hour, startTime.tm_min, endTime.tm_hour, endTime.tm_min, chanel['title'])
-		window.getControl(5).setText(txtProgram)
-		window.getControl(1).setLabel(param['title'])
-	except Exception, e:
-		showMessage('Error', e, 5000)
-
-def GetChanels (params):
-	data = GetJsonChannels('', False)
-	if params['id'] != '203':
-		data = filter(lambda x: x['category_id'] == params['id'], data['channels'])
-		for chanel in data:
-			li = xbmcgui.ListItem('%s' % chanel['title'], chanel['title'], chanel['icon'])
-			uri = construct_request({
-				'func': 'play_ch',
-				'img': chanel['icon'].encode('utf-8'),
-				'title': chanel['title'].encode('utf-8'),
-				'file': base64.urlsafe_b64decode(chanel['id'].replace('?', 'L').replace('|', 'M').encode('utf-8'))
-			})
-			li.addContextMenuItems([('Телепрограмма', 'XBMC.RunPlugin(%s?func=GetProgram&id=%s&title=%s)' % (sys.argv[0], chanel['number'], chanel['title']),)])
-			xbmcplugin.addDirectoryItem(hos, uri, li, False)
+			sch = db.GetSchedules(params['id'])
+		except:
+			showMessage(message = 'Программа загружается')
+			xbmc.sleep(2000)
+			GetScript(params)
+			return
 	else:
-		data = filter(lambda x: x['hd'] == "1", data['channels'])
-		for chanel in data:
-			li = xbmcgui.ListItem('%s' % chanel['title'], chanel['title'], chanel['icon'])
-			uri = construct_request({
-				'func': 'play_ch',
-				'img': chanel['icon'].encode('utf-8'),
-				'title': chanel['title'].encode('utf-8'),
-				'file': base64.urlsafe_b64decode(chanel['id'].replace('?', 'L').replace('|', 'M').encode('utf-8'))
-			})
-			li.addContextMenuItems([('Телепрограмма', 'XBMC.RunPlugin(%s?func=GetProgram&id=%s)' % (sys.argv[0], chanel['number']),)])
-			xbmcplugin.addDirectoryItem(hos, uri, li, False)
-			
-			
+		del db
+		return
+	del db
+	
+	for ch in sch:
+		txtProgram = ''
+		if not ch.has_key('program'):
+			xbmc.sleep(13)
+			window.getControl(1).setLabel(params['title'])
+			window.getControl(5).setText('Нет программы')
+			return
+		for item in ch['program']:
+			startTime = time.localtime(float(item['start']))
+			endTime = time.localtime(float(item['end']))
+			txtProgram = txtProgram + '%.2d:%.2d - %.2d:%.2d: %s\n' % (startTime.tm_hour, startTime.tm_min, endTime.tm_hour, endTime.tm_min, item['title'])
+		
+		xbmc.sleep(13)
+		window.getControl(1).setLabel(ch['name'])
+		window.getControl(5).setText(txtProgram)
+	
+	
+		
+def GetChannelsDB (params):
+	db = DataBase(db_name)
+	channels = None
+	if not params.has_key('group'):
+		return
+	elif params['group'] == '0':
+		channels = db.GetChannels()
+	elif params['group'] == 'hd':
+		channels = db.GetChannelsHD()
+	elif params['group'] == 'latest':
+		channels = db.GetLatestChannels()
+	elif params['group'] == 'new':
+		channels = db.GetNewChannels()
+	else:
+		channels = db.GetChannels(params['group'])
+	import time
+	for ch in channels:
+		img = ch['imgurl']
+		if __addon__.getSetting('logopack') == 'true':
+			logo_path = os.path.join(PLUGIN_DATA_PATH, 'logo')
+			logo_src = os.path.join(logo_path, ch['name'].decode('utf-8') + '.png')
+			if os.path.exists(logo_src):
+				img = logo_src
+		try:
+			sch = db.GetSchedules(ch['id'], 6, int(time.time()))
+		except:
+			GetChannelsDB(params)
+			return
+		if sch[0].has_key('program'):
+			startTime = time.localtime(float(sch[0]['program'][0]['start']))
+			endTime = time.localtime(float(sch[0]['program'][0]['end']))
+			title = '%s [COLOR FF0091E7]%.2d:%.2d - %.2d:%.2d: %s[/COLOR]' % (ch['name'], startTime.tm_hour, startTime.tm_min, endTime.tm_hour, endTime.tm_min, sch[0]['program'][0]['title'].encode('utf-8'))
+		else:
+			title = ch['name']
+		if params['group'] == '0' or params['group'] == 'hd' or params['group'] == 'latest' or params['group'] == 'new':
+			title = '[COLOR FF7092BE]%s:[/COLOR] %s' % (ch['group_name'], title)
+		li = xbmcgui.ListItem(title, title, img, img)
+		if sch[0].has_key('program'):
+			prog = ''
+			for item in sch[0]['program']:
+				startTime = time.localtime(float(item['start']))
+				endTime = time.localtime(item['end'])
+				prog = prog + '%.2d:%.2d - %.2d:%.2d: %s\n' % (startTime.tm_hour, startTime.tm_min, endTime.tm_hour, endTime.tm_min, item['title'].encode('utf-8'))
+				li.setInfo(type = "Video", infoLabels = {"Title": ch['name'], 'year': endTime.tm_year, 'genre': ch['group_name'], 'plot': '%s' % prog} )
+		else:
+			li.setInfo(type = "Video", infoLabels = {"Title": ch['name'], 'year': time.localtime().tm_year, 'genre': ch['group_name']})
+		li.setProperty('fanart_image', img.encode('utf-8'))
+		uri = construct_request({
+			'func': 'play_ch_db',
+			'img': img.encode('utf-8'),
+			'title': ch['name'],
+			'file': ch['urlstream'],
+			'id': ch['id']
+		})
+		deluri = construct_request({
+			'func': 'DelChannel',
+			'id': ch['id']
+		})
+		commands = []
+		commands.append(('Телепрограмма', 'XBMC.RunPlugin(%s?func=GetScript&id=%s&title=%s)' % (sys.argv[0], ch['id'], ch['name']),))
+		commands.append(('Удалить канал', 'XBMC.RunPlugin(%s)' % (deluri),))
+		li.addContextMenuItems(commands)
+		xbmcplugin.addDirectoryItem(hos, uri, li)
 	xbmcplugin.endOfDirectory(hos)
+	del db
+	
+def DelChannel(params):
+	db = DataBase(db_name)
+	db.DelChannel(params['id'])
+	showMessage(message = 'Канал удален')
+	xbmc.executebuiltin("Container.Refresh")
+	del db
+
+def play_ch_db(params):
+	url = params['file']
+	if url != '':
+		TSPlayer = tsengine()
+		out = None
+		if url.find('http://') == -1:
+			out = TSPlayer.load_torrent(url,'PID',port=aceport)
+		else:
+			out = TSPlayer.load_torrent(url,'TORRENT',port=aceport)
+		if out == 'Ok':
+			TSPlayer.play_url_ind(0,params['title'],addon_icon,params['img'])
+			db = DataBase(db_name)
+			db.IncChannel(params['id'])
+			del db
+		TSPlayer.end()
+		showMessage('Torrent', 'Stop')
+
+def GetParts():
+	db = DataBase(db_name)
+	parts = db.GetParts()
+		
+	for part in parts:
+		li = xbmcgui.ListItem(part['name'])
+		uri = construct_request({
+			'func': 'GetChannelsDB',
+			'group': part['id'],
+		})
+		xbmcplugin.addDirectoryItem(hos, uri, li, True)
 
 def mainScreen(params):
-	try:
-		data = GetJsonChannels('', True)
-		for group in data['types']:
-			pass
-			li = xbmcgui.ListItem('%s' % group['title'], group['title'])
-			uri = construct_request({
-				'func' : 'GetChanels',
-				'title' : group['title'].encode('utf-8'),
-				'id' : group['id']
-			})
-			xbmcplugin.addDirectoryItem(hos, uri, li, True)
-		xbmcplugin.endOfDirectory(hos)
-	except Exception, e:
-		showMessage('Raketa-TV', e)
+	li = xbmcgui.ListItem('[COLOR FF00FF00]Все каналы[/COLOR]')
+	uri = construct_request({
+		'func': 'GetChannelsDB',
+		'title': 'Все каналы',
+		'group': '0'
+	})
+	xbmcplugin.addDirectoryItem(hos, uri, li, True)
+	li = xbmcgui.ListItem('[COLOR FF00FF00]Последние просмотренные[/COLOR]')
+	uri = construct_request({
+		'func': 'GetChannelsDB',
+		'title': 'Последние просмотренные',
+		'group': 'latest'
+	})
+	xbmcplugin.addDirectoryItem(hos, uri, li, True)
+	li = xbmcgui.ListItem('[COLOR FF00FF00]HD Каналы[/COLOR]')
+	uri = construct_request({
+		'func': 'GetChannelsDB',
+		'title': 'HD Каналы',
+		'group': 'hd'
+	})
+	xbmcplugin.addDirectoryItem(hos, uri, li, True)
+	li = xbmcgui.ListItem('[COLOR FF00FF00]Новые каналы[/COLOR]')
+	uri = construct_request({
+		'func': 'GetChannelsDB',
+		'title': 'Новые каналы',
+		'group': 'new'
+	})
+
+	xbmcplugin.addDirectoryItem(hos, uri, li, True)
+	GetParts()
+	xbmcplugin.endOfDirectory(hos)
 	
 from urllib import unquote, quote, quote_plus
 def get_params(paramstring):
@@ -245,6 +271,27 @@ def get_params(paramstring):
 		for cur in param:
 			param[cur] = urllib.unquote_plus(param[cur])
 	return param
+	
+from database import DataBase
+
+db = DataBase(db_name)		
+dbver = db.GetDBVer()
+if db.GetDBVer() <> 5:
+	del db
+	os.remove(db_name)
+
+db = DataBase(db_name)
+
+lupd = db.GetLastUpdate()
+if lupd == None:
+	db.UpdateDB()
+else:
+	nupd = lupd + datetime.timedelta(hours = 168)
+
+	if nupd < datetime.datetime.now():
+		db.UpdateDB()
+db.UpdateSchedules()
+del db
 
 def addon_main():
 	params = get_params(sys.argv[2])
@@ -252,8 +299,12 @@ def addon_main():
 		func = params['func']
 		del params['func']
 	except:
+		if not os.path.exists(PLUGIN_DATA_PATH):
+			os.makedirs(PLUGIN_DATA_PATH)
+		
 		func = None
 		xbmc.log( '[%s]: Primary input' % addon_id, 1 )
+
 		mainScreen(params)
 	if func != None:
 		try: pfunc = globals()[func]
