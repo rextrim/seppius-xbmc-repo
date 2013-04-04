4f2fa755e250d29cdfd39bef65af604a3a02c41d
nuismons@gmail.com <nuismons@gmail.com@6a205204-3e34-5d9b-f158-6e7a6818afab>
2013-04-04 08:45:00 +0000

    ts 0.3.0

--- trunk/addons/script.module.torrent.ts/lib/TSEProgress.py
+++ trunk/addons/script.module.torrent.ts/lib/TSEProgress.py
@@ -2,44 +2,40 @@ import os
 import sys
 import re
 from traceback import print_exc
-
 import xbmc
-
 import xbmcgui
-
 import xbmcaddon
 
 settings = xbmcaddon.Addon(id='script.module.torrent.ts')
 language = settings.getLocalizedString
-
 addonDir = settings.getAddonInfo("path")
-print addonDir
-XBMC_SKIN  = xbmc.getSkinDir()
+#XBMC_SKIN  = xbmc.getSkinDir()
 
-
-class xbmcguiWindowError(Exception):
-	def __init__(self, winError=None):
-		Exception.__init__(self, winError)
-
-	  
-class WTSEngine(xbmcgui.WindowXMLDialog):
-	def __init__(self, *args, **kwargs):
-		#xbmcgui.WindowXMLDialog.__init__(self, *args, **kwargs)
-		#self.doModal()
-		self.progress=0
+  
+class WASEngine(xbmcgui.WindowXMLDialog):
+	def __init__(self, strXMLname, strFallbackPath, strDefaultName, forceFallback = True):
 		self.isCanceled=False
+		self.showCancel=True
 		
 		pass
 	def onInit(self):
+		self.progress=0
+		
 		self.controls = {}
-		try:
-			self.getControls()
-		except:
-			print_exc()
+		#try:
+	#		self.getControls()
+	#	except:
+	#		print_exc()
 		self.button = self.getControl(2005)
 		self.button.setLabel(language(1006))
 		self.setFocus(self.getControl(2005))
-		#self.close()
+		self.label = self.getControl(2002)
+		self.label.setLabel('')
+		self.label = self.getControl(2003)
+		self.label.setLabel('')
+		self.button = self.getControl(2005)
+		self.button.setLabel(language(1006))
+
 	def updater(self,progress,text1,text2):
 		self.label = self.getControl(2004)
 		self.label.setPercent(progress)
@@ -49,43 +45,19 @@ class WTSEngine(xbmcgui.WindowXMLDialog):
 		self.label.setLabel(text2)
 		self.button = self.getControl(2005)
 		self.button.setLabel(language(1006))
+		self.button.setVisible(self.showCancel)
 		self.setFocus(self.getControl(2005))
-	def getControls(self):
-		pass
-
-	def onFocus(self, controlID):
-		pass
 
 	def onClick(self, controlID):
-		if controlID==2005: self.isCanceled=True
-		pass
-	def show1(self):
-		self.label = self.getControl(2000)
-		self.label.setVisible(True)
-	def hide1(self):
-		self.label = self.getControl(2000)
-		self.label.setVisible(False)
-	def onAction(self, action):
-		pass
+		if controlID==2005: 
+			self.isCanceled=True
+
 
 class dwprogress():
 	def __init__(self, *args, **kwargs):
-		self.ui = WTSEngine("DialogDownloadProgress.xml",addonDir,'default')
-		#self.ui.doModal()
+		self.ui = WASEngine("DialogDownloadProgress.xml",addonDir, "default")
 		self.ui.show()
-	def onInit(self):
-		#self.ui = WTSEngine("DialogDownloadProgress.xml",addonDir)
-		#self.ui.show()
-		pass
-	def show(self):
-		self.ui.show1()
-		pass
-	def hide(self):
-		self.ui.hide1()
-		pass
 	def updater(self,progress=0,text1='',text2=''):
 		self.ui.updater(progress,text1,text2)
-		#self.ui.update()
-		pass
 	def close(self):
 		self.ui.close()
